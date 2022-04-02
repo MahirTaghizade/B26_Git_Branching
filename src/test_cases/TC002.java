51789041a5fc8dbdf957db196d27119e1be08daa
mahirtaghizade <mahir95.x@icloud.com>
2022-04-02 14:40:19 -0400

    TC002 updated

--- src/test_cases/TC002.java
+++ src/test_cases/TC002.java
@@ -4,6 +4,9 @@ public class TC002 {
     public static void main(String[] args) {
 
         // Test case 2
+        // Writing smoke test
+        // creating a report
+        // screenshot in failure
 
     }
 }
